d9b18dca8dec86004efd40800215133c44609627
Ryan Sisco <ryanpsisco@gmail.com>
2017-06-14 15:46:16 -0700

    Add files via upload

---
+++ public/postTemplate.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['post'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<article class=\"post\">\r\n  <div class=\"post-content\">\r\n    <p class=\"upvote\">\r\n      <i class=\"fa fa-arrow-up\" aria-hidden=\"true\"></i>\r\n    </p>\r\n    <p class=\"points\">\r\n      -23\r\n    </p>\r\n    <p class=\"downvote\">\r\n      <i class=\"fa fa-arrow-down\" aria-hidden=\"true\"></i>\r\n    </p>\r\n    <p class=\"text\">\r\n      Help, I'm still in silver\r\n    </p>\r\n    <p class=\"author\">\r\n      <a href=\"#\">newusr322</a>\r\n    </p>\r\n  </div>\r\n</article>";
+},"useData":true});
+})();
